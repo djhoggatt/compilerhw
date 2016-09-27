08fbcdfa338a3bd4eed8a182f3d825a7c699fa8b
djhoggatt <djhoggatt@gmail.com>
2016-09-27 17:44:05 -0600

    Update hw2_4.py

--- hw2_4.py
+++ hw2_4.py
@@ -1,7 +1,9 @@
 #Main function used to test
 def main():
 	#Keep in mind, python flips the bit's index
-	print "Goodbye"
+	#add some stuff
+	print "Hello"
+	print "Another print"
 	print "Testing With AVVs 0000 and 0000"
 	print unsignedsub_4bit(['0','0','0','0'],['0','0','0','0'])
 	print "\n"
